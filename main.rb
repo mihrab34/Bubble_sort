0b06ee67f747918b444a64e98c58d5ad5ea26b5a
Samuel <babssam69@gmail.com>
2019-12-05 20:22:30 +0000

    added bubble_sort_by method

--- main.rb
+++ main.rb
@@ -16,24 +16,24 @@ def bubbleSort arr
 
 bubbleSort([4,3,78,2,0,2])
 
-# def bubble_sort_by arr
-#     num = arr.length
+def bubble_sort_by arr
+    num = arr.length
 
-#     loop do
-#     sorted = false
-#     (num-1).times do |i|
-#     if yield(arr[i], arr[i+1]).positive? 
-#       arr[i], arr[i + 1] = arr[i + 1], arr[i]
-#       sorted = true
-#           end
-#         end
-#         break if not sorted
-#       end
-#       arr
-#     end
+    loop do
+    sorted = false
+    (num-1).times do |i|
+    if yield(arr[i], arr[i+1]).positive? 
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      sorted = true
+          end
+        end
+        break if not sorted
+      end
+      arr
+    end
 
-# bubble_sort_by(["hi","hello","hey"]) do |left,right|
-#   left.length - right.length
-# end
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
 
 
